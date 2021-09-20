19e4147cd6f1edf3fdb046376aea83774926dbbe
OAMichael <70339407+OAMichael@users.noreply.github.com>
2021-09-20 19:35:20 +0300

    Update 5fchown.c

--- 5fchown.c
+++ 5fchown.c
@@ -36,12 +36,12 @@ void copying(const char* first, const char* second)
     }
 
     if((sb.st_mode & S_IFMT) != S_IFREG) {
-    	fprintf(stderr, "Copied file is not 'Regular' type\n");			// Failure if file is not type of regular
+    	fprintf(stderr, "Copied file is not 'Regular' type\n");				// Failure if file is not type of regular
     	exit(EXIT_FAILURE);
     }
 
 	int fd1, fd2;
-	fd1 = open(first, O_RDONLY);										// Opening the first file
+	fd1 = open(first, O_RDONLY);							// Opening the first file
 	if (fd1 < 0) 
 	{
 		perror("Failed to open copied file");
@@ -59,41 +59,41 @@ void copying(const char* first, const char* second)
 	ssize_t tst = 1;
 	while(tst)
 	{
-		void* buf = calloc(4096, sizeof(char));    						// allocation 4Kb (blocksize) of memory
-		tst = read(fd1, buf, 4096);               						// reading into this memory (buf)
+		void* buf = calloc(4096, sizeof(char));    				// allocation 4Kb (blocksize) of memory
+		tst = read(fd1, buf, 4096);               				// reading into this memory (buf)
 		if(tst < 0) {
 			perror("Failed to read file");
-			free(buf);													// free memory in case of failure
+			free(buf);							// free memory in case of failure
 			exit(EXIT_FAILURE);
 		}
 
-		if(writeall(fd2, buf, 4096) < 0)          						// writing into second file from buf
+		if(writeall(fd2, buf, 4096) < 0)          				// writing into second file from buf
 		{
 			perror("Failure while writing");
-			free(buf);													// free memory in case of failure
+			free(buf);							// free memory in case of failure
 			exit(EXIT_FAILURE);
 		}
 		free(buf);
 	}
 
-	if(fchown(fd2, sb.st_uid, sb.st_gid) < 0) {							// copying User Id and Group Id
+	if(fchown(fd2, sb.st_uid, sb.st_gid) < 0) {					// copying User Id and Group Id
 		perror("Failure while copying UID and GID");
 		exit(EXIT_FAILURE);
 	}
 
 
-	if(fchmod(fd2, sb.st_mode) < 0) {									// copying access permissions
+	if(fchmod(fd2, sb.st_mode) < 0) {						// copying access permissions
 		perror("Failure while copying access permissions");
 		exit(EXIT_FAILURE);
 	}
 
-	struct timespec times[2];											// creating timespec structure
-	times[0].tv_sec = sb.st_atim.tv_sec;								// for futimens(int fd, struct timespec times)
+	struct timespec times[2];							// creating timespec structure
+	times[0].tv_sec = sb.st_atim.tv_sec;						// for futimens(int fd, struct timespec times)
 	times[0].tv_nsec = sb.st_atim.tv_nsec;
 	times[1].tv_sec = sb.st_mtim.tv_sec;
 	times[1].tv_nsec = sb.st_mtim.tv_nsec;
 
-	if(futimens(fd2, times) < 0) {										// copying atime and mtime of file
+	if(futimens(fd2, times) < 0) {							// copying atime and mtime of file
 		perror("Failure while copying times");
 		exit(EXIT_FAILURE);
 	}
