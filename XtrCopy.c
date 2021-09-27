b53b003fd35b578b61a7111db62b73b13bc2977f
OAMichael <70339407+OAMichael@users.noreply.github.com>
2021-09-27 23:37:42 +0300

    Update XtrCopy.c

--- XtrCopy.c
+++ XtrCopy.c
@@ -10,14 +10,14 @@
 #include <unistd.h>
 
 ssize_t pwriteall(int fd, const void* buf, size_t count, int offset);
-void copying(const char* first, const char* second);					// copy function
-void CrtLink(const char* pathname, const char* newname);				// function which creates a new symlink
+void copying(const char* first, const char* second);						// copy function
+void CrtLink(const char* pathname, const char* newname);					// function which creates a new symlink
 
 
 int main(int argc, char* argv[])
 {
 	if(argc != 3) {
-		fprintf(stderr, "Insufficient amount of arguments\n");			// Failure in case of insufficient amount of arguments
+		fprintf(stderr, "Insufficient amount of arguments\n");				// Failure in case of insufficient amount of arguments
 		exit(EXIT_FAILURE);
 	}
 
@@ -28,7 +28,7 @@ int main(int argc, char* argv[])
        exit(EXIT_FAILURE);
     }
 
-    switch(sb.st_mode & S_IFMT) {										// checking file type
+    switch(sb.st_mode & S_IFMT) {								// checking file type
 	case S_IFREG:	copying(argv[1], argv[2]); break;					// just copying file if original one is regular type	
 
 	case S_IFIFO:	if(mkfifo(argv[2], S_IFIFO) < 0) {					// creating FIFO file if original one is FIFO/pipe
@@ -39,7 +39,7 @@ int main(int argc, char* argv[])
 	case S_IFLNK:   CrtLink(argv[1], argv[2]); 	break;					// creating a new link if argv[1] is symlink
 
 	case S_IFBLK:   if(mknod(argv[2], sb.st_mode, sb.st_rdev) < 0) {
-						fprintf(stderr, "Failed to create new block device");      		// block device
+						fprintf(stderr, "Failed to create new block device");      	// block device
 						exit(EXIT_FAILURE);     
 					} 	break;												
 
@@ -59,7 +59,7 @@ void copying(const char* first, const char* second)
 {
 
 	int fd1, fd2;
-	fd1 = open(first, O_RDONLY);										// Opening the first file
+	fd1 = open(first, O_RDONLY);							// Opening the first file
 	if (fd1 < 0) {
 		fprintf(stderr, "Failed to open %s\n", first);
 		exit(EXIT_FAILURE);
@@ -75,21 +75,21 @@ void copying(const char* first, const char* second)
 	ssize_t tst = 1;
 	while(tst)
 	{
-		void* buf = calloc(4096, sizeof(char));    						// allocation 4Kb (blocksize) of memory
-		tst = pread(fd1, buf, 4096, offset);               				// reading into this memory (buf)
+		void* buf = calloc(4096, sizeof(char));    				// allocation 4Kb (blocksize) of memory
+		tst = pread(fd1, buf, 4096, offset);               			// reading into this memory (buf)
 		if(tst < 0) {
 			fprintf(stderr, "Failed to read %s\n", first);
-			free(buf);													// free memory in case of failure
+			free(buf);							// free memory in case of failure
 			exit(EXIT_FAILURE);
 		}
 
-		if(pwriteall(fd2, buf, 4096, offset) < 0)          				// writing into second file from buf
+		if(pwriteall(fd2, buf, 4096, offset) < 0)          			// writing into second file from buf
 		{
 			fprintf(stderr, "Failure while writing to %s\n", second);
-			free(buf);													// free memory in case of failure
+			free(buf);							// free memory in case of failure
 			exit(EXIT_FAILURE);
 		}
-		offset += 4096;													// moving offset
+		offset += 4096;								// moving offset
 		free(buf);
 	}
 
@@ -123,17 +123,17 @@ ssize_t pwriteall(int fd, const void* buf, size_t count, int offset)
 
 void CrtLink(const char* pathname, const char* newname)
 {
-	char* buf = (char*)calloc(strlen(pathname) + 4, sizeof(char));		// memory allocation
+	char* buf = (char*)calloc(strlen(pathname) + 4, sizeof(char));			// memory allocation
 
 	if(readlink(pathname, buf, strlen(pathname) + 4) < 0) {				// reading the link
 		fprintf(stderr, "Failed to read link\n");
-		free(buf);														// free memory in case of failure
+		free(buf);								// free memory in case of failure
 		exit(EXIT_FAILURE);
 	}
 
-	if (symlink(buf, newname) < 0) {                                    // creating new link
+	if (symlink(buf, newname) < 0) {                                    		// creating new link
 		fprintf(stderr, "Failed to create a  new link\n");
-		free(buf);														// free memory in case of failure
+		free(buf);								// free memory in case of failure
 		exit(EXIT_FAILURE);
 	}
 }
